3a89cca1389d95f83bf40ea7167c18b7441336d7
Thidas Senavirathna <thidaskaveesha@gmail.com>
2025-04-17 08:14:13 +0530

    ui: added game page ui

--- Frontend/src/pages/gamePage.jsx
+++ Frontend/src/pages/gamePage.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import { FaArrowLeft, FaHandRock, FaHandPaper, FaHandScissors  } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import "../styles/global.css";
 
 function GamePage() {
+    const navigate = useNavigate();
+    const DEFAULT_SESSION_ID = "XXXXX"; // Default value for sessionID
+
+    const navigateToMenu = () => {
+        navigate("/");
+    };
+
+    let sessionID = localStorage.getItem("sessionID") || DEFAULT_SESSION_ID;
+
     return (
-        <div className="match-page">
-            <h1>Match Page</h1>
-            <p>This is the match page where players can see their matches.</p>
+        <div className="match-container">
+            <div className="header">
+                <button 
+                    className="btn" 
+                    onClick={navigateToMenu} 
+                    aria-label="Go to menu"
+                >
+                    <FaArrowLeft  className="icon" />
+                </button>
+                <h1>Game ID: {sessionID}</h1>
+            </div>
+            <div className="content">
+                <h1>Time Left</h1>
+                <h2>00:00</h2>
+                <p>Make your choice ?</p>
+            </div>
+            <div className="choice">
+                <FaHandRock  className="icon" />
+                <FaHandPaper  className="icon" />
+                <FaHandScissors  className="icon" />
+            </div>
         </div>
     );
 }
